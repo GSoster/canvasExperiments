67f2e0d473edb5ee64ace28ef72c26ffb1c8490a
Guilherme Soster <guilhermesosters@gmail.com>
2016-12-08 16:45:38 -0200

    added some more code

--- blockItGame/game.js
+++ blockItGame/game.js
@@ -55,5 +55,72 @@ var ball = {
   blockedtime = blockcooldown;
   gameover - trye;
   gameovertime = 0;
+};
 
- }
+function updateGame(dt) {
+  // Move the ball, time-based
+  ball.x += dt * ball.speed * ball.xdir;
+  ball.y += dt * ball.speed * ball.ydir;
+  // Open the wall after some time
+  blockedtime += dt;
+  if (blocked) {
+    if (blockedtime > blockedlength) {
+      blocked = false;
+    }
+  }
+  // Handle left and right collisions with the level
+  if (ball.x <= level.x) {
+    // Left edge
+    ball.xdir *= -1;
+    ball.x = level.x;
+  } else if (ball.x + ball.width >= level.x + level.width) {
+    // Right edge
+    ball.xdir *= -1;
+    ball.x = level.x + level.width - ball.width;
+  }
+  // Handle top collisions with the level
+  if (ball.y <= level.y) {
+    // Top edge
+    ball.ydir *= -1;
+    ball.y = level.y;
+  }
+  // Check if the ball collides with the bottom wall
+  // Only do this, while the ball is moving to the bottom
+  if (blocked && ball.ydir > 0 && rectIntersection(ball.x, ball.y, ball.width, ball.height, level.x, level.y+level.height, level.width, level.wallsize) && ball.ydir > 0) {
+    // Wall is closed
+    ball.ydir *= -1;
+    // Increase the score
+    score += 1;
+    // Increase the speed of the ball by 5 percent
+    ball.speed *= 1.05;
+  } else if (ball.ydir > 0 && ball.y > level.y+level.height+level.wallsize) {
+    // Ball escaped, game over
+    ball.speed = 0;
+    gameover = true;
+  }
+};
+
+ // Check if there is a rect intersection
+function rectIntersection(x1, y1, w1, h1, x2, y2, w2, h2) {
+  if (x1 <= x2 + w2 && x1 + w1 >= x2 && y1 <= y2 + h2 && y1 + h1 >= y2) {
+    return true;
+  }
+  return false;
+}
+
+// Mouse event handlers
+function onMouseDown(e) {
+  // Get the mouse position
+  var pos = getMousePos(canvas, e);
+
+  // Start a new game
+  if (gameover && gameovertime > gameoverdelay) {
+    newGame();
+    gameover = false;
+  }
+  // Show the wall
+  if (!gameover && blockedtime >= blockcooldown) {
+    blockedtime = 0;
+    blocked = true;
+  }
+};
