97abbeac850358788f0c8a1b810f65f65b151fb5
Guilherme Soster <GSoster@users.noreply.github.com>
2018-07-12 16:06:56 -0300

    setting up canvasGrid for farm project

---
+++ farmCanvas/farm.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 4;
+
+const squareSize = 60;
+const canvasSize = 600; //assuming it is a square
+for (var i = 0; i < canvasSize; i+= squareSize)
+{
+    
+}
