ff50bf4e6d0fe31756a3c3008e885819a8f53ffb
Guilherme Soster <GSoster@users.noreply.github.com>
2018-07-16 12:02:13 -0300

    added methods to move and draw the animal

--- farmCanvas/Animal.js
+++ farmCanvas/Animal.js
@@ -1,10 +1,12 @@
 var Animal = class 
 {
 
-    constructor(currentX, currentY)
+    constructor(currentX, currentY, width = 30, height = 30)
     {
         this.currentX = currentX;
-        this.currentY = currentY;        
+        this.currentY = currentY;
+        this.width = width;
+        this.height = height;
     }
 
     /**
@@ -15,6 +17,27 @@ var Animal = class
     MoveHorizontally(distance)
     {
         this.currentX += distance;
+    }
+
+    /**
+     * Distance can be a positive int (move down)
+     * or a negative int (move up)
+     * @param {int} distance 
+     */
+    MoveVertically(distance)
+    {
+        this.currentY += distance;
+    }
+
+    
+     //* Renders the graphic representation of this objetc
+     
+    Draw(canvasContext)
+    {
+        canvasContext.save();
+        canvasContext.fillStyle="#FF0000";
+        canvasContext.fillRect(this.currentX, this.currentY, this.width, this.height);
+        canvasContext.restore();
     }
 
 }
