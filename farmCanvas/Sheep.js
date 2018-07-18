694bcb298572fa1f6be68cb6d37c6b4434550ff0
GSoster <guilhermesosters@gmail.com>
2018-07-18 00:18:00 -0300

    sheep is no displaying an image of an actual sheep

--- farmCanvas/Sheep.js
+++ farmCanvas/Sheep.js
@@ -3,10 +3,15 @@ class Sheep extends Animal
     constructor (name, currentX, currentY)
     {
         super(name, currentX, currentY);
-        this.image = new Image();
-        this.image.src = "images/frame-sprite-animation.png";
+        var img = new Image();
         this.isImageLoaded = false;
-        this.image.onload = function (){this.isImageLoaded = true;}
+        img.onload = function(){            
+            this.isImageLoaded = true;
+            alert("FOI? " + this.isImageLoaded);
+        }
+        img.src = "./images/sheep_walk.png";
+        this.image = img;
+        this.spriteSize = 110;
     }
 
     /**
@@ -21,15 +26,16 @@ class Sheep extends Animal
         canvasContext.fillRect(this.currentX, this.currentY, this.width, this.height);
         canvasContext.restore();
 
-        if (this.isImageLoaded)
-        {
-            console.log("Image loaded!");
-            canvasContext.drawImage(this.image, 0, 0, 40, 40, this.currentX, this.currentY, 40, 40);
-        }
-        else
-        {
-            console.log("Image didn't load!");
-        }
+        //if (this.isImageLoaded)
+        //{
+            //console.log("Image loaded!");
+            console.log(this.spriteSize);
+            canvasContext.drawImage(this.image, 0, 0, this.spriteSize, this.spriteSize, this.currentX, this.currentY, this.spriteSize, this.spriteSize);
+        //}
+        //else
+        //{
+          //  console.log("Image didn't load!");
+        //}
 
 
     }
