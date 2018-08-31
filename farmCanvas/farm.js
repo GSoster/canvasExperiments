26f360e7ddc89d17429244017a074f26ca36da60
Guilherme Soster <GSoster@users.noreply.github.com>
2018-08-31 14:31:33 -0300

    Working with animals list instead of single entities

--- farmCanvas/farm.js
+++ farmCanvas/farm.js
@@ -11,6 +11,12 @@ var board = new Board(600, 600);
 var animal = new Animal('Animal', 20, 40);
 var sheep = new Sheep('Sheep', 120, 90);
 var chicken = new Chicken('Chicken', 30, 50);
+
+var farmAnimals = Array();
+farmAnimals.push(animal);
+farmAnimals.push(sheep);
+farmAnimals.push(chicken);
+
 /**
  * Updates Graphically all elements 
  * @param {CanvasRenderingContext2D} canvasContext 
@@ -20,18 +26,21 @@ function GraphicUpdate (canvasContext)
     //console.log("Graphic Update");
     board.Clear(canvasContext);
     board.Draw(canvasContext);
-    animal.Draw(canvasContext);
+    
+    /*animal.Draw(canvasContext);
     sheep.Draw(canvasContext);
-    chicken.Draw(canvasContext);
+    chicken.Draw(canvasContext);*/
+    farmAnimals.forEach( farmAnimal => farmAnimal.Draw(canvasContext));
 }
 
 
 function LogicUpdate ()
 {
     //console.log("Logic Update");
-    animal.Stray();
+    /*animal.Stray();
     sheep.Stray();
-    chicken.Stray();
+    chicken.Stray();*/
+    farmAnimals.forEach(farmAnimal => farmAnimal.Stray());
 
 }
 
